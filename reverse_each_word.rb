4bc9c9b67f169c8bd6165e5069180d5e1138c3cd
Steven Ross <ross.steven@me.com>
2020-04-18 16:16:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(sentence)
-  reversed = (sentence.split.map do |word|
-    word.reverse_each_word
-  end).join(' ')
+  sentence.split = new_sentence
+  new_sentence.reverse
 end
