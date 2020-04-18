4a4d1470a9d3de30fad645766f4d699932b3ddcd
Steven Ross <ross.steven@me.com>
2020-04-18 16:45:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,9 @@ def reverse_each_word(sentence)
   array = []
   array << sentence.split
   array.each do |word|
-    word.reverse
+      word.reverse
+      array.reverse
+
   binding.pry
-end
+  end
 end
