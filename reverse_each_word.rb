398d2dcd4a9e1b0fada0ce05964b20b53fd4eb11
Steven Ross <ross.steven@me.com>
2020-04-18 17:03:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,5 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-  array = []
-  array << sentence.split
-  array.each do |word|
-      word.reverse
-      binding.pry
-  end
-  reversed_array
+  sentence.split.collect {|word| word.reverse}.join(' ')
 end
